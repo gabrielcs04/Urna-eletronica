65b8a866e0ff37a60356e67bf7a8f19676845079
Gabriel Costa <gabriel.costasilva04@gmail.com>
2021-03-19 19:49:35 -0300

    subindo projeto no GitHub

---
+++ etapas.js
@@ -0,0 +1,70 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '11111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    {
+                        url: '2.jpg',
+                        legenda: 'Vereador'
+                    }
+                ]
+            },
+            {
+                numero: '22222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEF',
+                fotos: [
+                    {
+                        url: '3.jpg',
+                        legenda: 'Vereador'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '11',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {
+                        url: '1.jpg',
+                        legenda: 'Prefeito',
+                    },
+                    {
+                        url: '1_2.jpg',
+                        legenda: 'Vice-Prefeito',
+                        small: true
+                    },
+                ]
+            },
+            {
+                numero: '22',
+                nome: 'Zulano',
+                partido: 'DEF',
+                vice: 'Zul',
+                fotos: [
+                    {
+                        url: '1_2.jpg',
+                        legenda: 'Prefeito',
+                    },
+                    {
+                        url: '1.jpg',
+                        legenda: 'Vice-Prefeito',
+                        small: true
+                    },
+                ]
+            }
+        ]
+    },
+]
